2d965b953dea9acec3fdd4a4d0fa07765607d2dd
sploicers <7642631+sploicers@users.noreply.github.com>
2026-05-06 00:02:14 +1200

    Client was missing call to client_connect() after refactor

--- crates/busman-client/src/main.rs
+++ crates/busman-client/src/main.rs
@@ -1,10 +1,16 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+
 use busman_core::{engine::Engine, result::Result};
 
 fn main() -> Result<()> {
 	env_logger::init();
+	let port = 9000;
+	let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
 	log::info!("Initializing...");
 
 	let mut engine = Engine::new()?;
+	engine.client_connect(addr)?;
+
 	let devices = engine.query_devices()?;
 	let selected_device = devices.first().expect("Must specify device to import");
 
